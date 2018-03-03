33b658a0e1fa9f8c16fef2809804afa7ceb52fa0
Chanthy Ngin <nginchanthy@gmail.com>
2018-03-03 15:29:20 -0800

    added more methods of  Dictionaries

--- makedictionary.py
+++ makedictionary.py
@@ -9,3 +9,22 @@ print "My name is", student["name"]
 print "My age is", student["age"]
 print "My country is", student["country"]
 print "My favorite language is", student["favorite_lang"]
+
+def print_dict(oDict):
+    for item in oDict:
+        print item
+    
+    for k in oDict.iterkeys():
+        print k
+    
+    for val in oDict.itervalues():
+        print val
+    
+    for key, data in oDict.iteritems():
+        print key, " = ", data
+
+    print oDict.items()
+    print oDict.keys()
+    print oDict.values()
+
+print_dict(student)
