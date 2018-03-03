7a85393793beef4f6848640b4690d085c1c5b1cb
Chanthy Ngin <nginchanthy@gmail.com>
2018-03-03 15:40:25 -0800

    Print Names from nested dictionaries

---
+++ printnames.py
@@ -0,0 +1,27 @@
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+for elem in students:
+    print elem["first_name"], elem["last_name"]
+
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for user in users:
+     print user
+     for idx in range(0, len(users[user])):
+        print idx+1, " - ", users[user][idx]["first_name"], users[user][idx]["last_name"], " - ", len(users[user][idx]["first_name"]+users[user][idx]["last_name"])
