8d794528b4dd6c8a16ad0fa724630e6d017902d2
Chanthy Ngin <nginchanthy@gmail.com>
2018-03-03 15:23:53 -0800

    added Make and using Dictionaries

---
+++ makedictionary.py
@@ -0,0 +1,11 @@
+student = {
+    "name": "Toto",
+    "age": 12,
+    "country": "USA",
+    "favorite_lang": "Python"
+}
+
+print "My name is", student["name"]
+print "My age is", student["age"]
+print "My country is", student["country"]
+print "My favorite language is", student["favorite_lang"]
